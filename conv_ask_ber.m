9d2315db302b7f54b2aa4bda119de1757a3206aa
Ser-Wang <101936269+Ser-Wang@users.noreply.github.com>
2025-03-05 17:11:48 +0800

    Significant upgrade
    Much more flexible

--- conv_ask_ber.m
+++ conv_ask_ber.m
@@ -1,4 +1,4 @@
-function [ber] = conv_ask_ber(info_bits, ask_depth, EbNo_dB_vec)
+function [ber] = conv_ask_ber(info_bits, ask_depth, EbNo_dB_vec, constraintLength, trellis)
 % 卷积码编译码仿真
 % clc;
 % clear;
@@ -9,30 +9,30 @@ function [ber] = conv_ask_ber(info_bits, ask_depth, EbNo_dB_vec)
 % codeRate = 1/2;         % 编码速率
 % EbNo_dB_vec = 0:2:10;   % 比特能量与噪声功率谱密度比（dB）
 % ask_depth = 1;
-
 % info_length = 100000;   % 信息序列长度
 % info_bits = randi([0, 1], info_length, 1); % 生成随机二进制信息序列
+% Trellis
+% constraintLength = 3;       % 卷积码约束长度
+% trellis = poly2trellis(constraintLength, [7 5]);    % 生成多项式为 [111, 101]，约束长度为 3
 
 ASK_amplitude_0 = 1 - ask_depth;
 ASK_amplitude_1 = 1;
+hard_threshold = 1 - ask_depth / 2;
 
 % Result storage
 ber = zeros(size(EbNo_dB_vec));
 
-% Encode
-constraintLength = 3;       % 卷积码约束长度
-trellis = poly2trellis(constraintLength, [7 5]);    % 生成多项式为 [111, 101]，约束长度为 3
-
+%% Encode
 x = convenc(info_bits, trellis);
 
-% ASK调制
-x_ask = zeros(size(x));     %预分配内存，变量会随i_x的迭代次数改变，与分配内存以提高运行速度
-
+% Modulation
+x_modulated = zeros(size(x));     %预分配内存，变量会随i_x的迭代次数改变，与分配内存以提高运行速度
+% ASK
 for i_x = 1 : length(x)
     if x(i_x) == 1
-        x_ask(i_x) = ASK_amplitude_1; % 幅度为1
+        x_modulated(i_x) = ASK_amplitude_1; % 幅度为1
     else
-        x_ask(i_x) = ASK_amplitude_0; % 幅度为0
+        x_modulated(i_x) = ASK_amplitude_0; % 幅度为0
     end
 end
 
@@ -44,14 +44,13 @@ for i_ebno = 1 : length(EbNo_dB_vec)
 %     noisePower = 1 / (2 * 10^(EbNo_dB(i_ebno)/10)); 
 %     noise = sqrt(noisePower) * randn(size(x)); 
 %     y_noisy_ask_100 = x_ask_100 + noise;
-    y_noisy = awgn(x_ask,EbNo_dB_vec(i_ebno),'measured');
+    y_noisy = awgn(x_modulated,EbNo_dB_vec(i_ebno),'measured');
 
     % Demodulate - Hard
-    hard_threshold = 1 - ask_depth / 2;
-    demodulated_y = (y_noisy > hard_threshold);
+    y_demodulated = (y_noisy > hard_threshold);
 
     % Decode
-    x_decoded = vitdec(demodulated_y, trellis, 5*constraintLength, 'trunc', 'hard');
+    x_decoded = vitdec(y_demodulated, trellis, 5*constraintLength, 'trunc', 'hard');
 
 %     [numErrors_ask_100, ber_ask_100(i_ebno)] = biterr(info_bits, decoded_ask_100);
     [~ , ber(i_ebno)] = biterr(info_bits, x_decoded);
--- main.m
+++ main.m
@@ -1,25 +1,43 @@
 clc;
 clear;
-
 % Parameters
-max_runs = 3000;
-
-info_length = 100000;   % 信息序列长度
+max_runs = 30;
+info_length = 256;   % 信息序列长度
 codeRate = 1/2;         % 编码速率
 EbNo_dB_vec = 0:2:10;   % 比特能量与噪声功率谱密度比（dB）
 
+
+%% Comparision Parameters
+num_comp = 4;
+
+% Variables
+c = 1;
+ask_depths(c) = 0.8;
+constraintLengths(c) = 3;                                   % 卷积码约束长度
+trelliss(c) = poly2trellis(constraintLengths(c), [7 5]);    % 生成多项式为 [111, 101]，约束长度为 3
+
+c = 2;
+ask_depths(c) = 0.8;
+constraintLengths(c) = 3;
+trelliss(c) = poly2trellis(constraintLengths(c), [7 5]); 
+
+c = 3;
+ask_depths(c) = 0.8;
+constraintLengths(c) = 3;
+trelliss(c) = poly2trellis(constraintLengths(c), [7 5]); 
+
+c = 4;
+ask_depths(c) = 0.8;
+constraintLengths(c) = 3;
+trelliss(c) = poly2trellis(constraintLengths(c), [7 5]); 
+
+% ber storage
+ber_sum = zeros(num_comp, length(EbNo_dB_vec));     %每行对应一个对比条件，列对应多个信噪比，命令行窗口打印时矩阵会转置
+
+% Print
 print_resolution = 5;
 print_matrix = zeros(length(EbNo_dB_vec),4);
 print_matrix(:,1) = EbNo_dB_vec';
-
-% Variables
-ask_depth_30  = 0.3;
-ask_depth_80  = 0.8;
-ask_depth_100 = 1;
-
-ber_sum_ask30 = zeros(size(EbNo_dB_vec));
-ber_sum_ask80 = zeros(size(EbNo_dB_vec));
-ber_sum_ask100 = zeros(size(EbNo_dB_vec));
 
 % 实时更新图形
 monitor_matrix = nan(max_runs / print_resolution);
@@ -31,31 +49,34 @@ hPlot30 = semilogy(print_resolution:print_resolution:max_runs, nan(max_runs / pr
 hold off;
 legend('ASK 30%');
 title('实时平均 BER 曲线');
-xlabel('Eb/No (dB)');
+xlabel('Run times');
 ylabel('BER');
 grid on;
 
+%% Begin
+ber = zeros(num_comp, length(EbNo_dB_vec));
 for i_runs = 1 : max_runs
     info_bits = randi([0, 1], info_length, 1); % 生成随机二进制信息序列
 
-    [ber_ask_30] = conv_ask_ber(info_bits, ask_depth_30, EbNo_dB_vec);
-    [ber_ask_80] = conv_ask_ber(info_bits, ask_depth_80, EbNo_dB_vec);
-    [ber_ask_100] = conv_ask_ber(info_bits, ask_depth_100, EbNo_dB_vec);
+%     [ber(1, :)] = conv_ask_ber(info_bits, ask_depth_30, EbNo_dB_vec, constraintLength, trellis);
+%     [ber(2, :)] = conv_ask_ber(info_bits, ask_depth_80, EbNo_dB_vec, constraintLength, trellis);
+%     [ber(3, :)] = conv_ask_ber(info_bits, ask_depth_100, EbNo_dB_vec, constraintLength, trellis);
 
-    ber_sum_ask30 = ber_sum_ask30 + ber_ask_30;
-    ber_sum_ask80 = ber_sum_ask80 + ber_ask_80;
-    ber_sum_ask100 = ber_sum_ask100 + ber_ask_100;
+    for i_comp = 1 : num_comp   % i_comp指第i个对比条件，对应上面的第i行函数调用
+        [ber(i_comp, :)] = conv_ask_ber(info_bits, ask_depths(i_comp), EbNo_dB_vec, constraintLengths(i_comp), trelliss(i_comp));
+        ber_sum(i_comp, :) = ber_sum(i_comp, :) + ber(i_comp, :);
+    end
 
     % Print
     if(mod(i_runs,print_resolution) == 0)
-        print_matrix(:, 2) = ber_sum_ask30'./i_runs;
-        print_matrix(:, 3) = ber_sum_ask80'./i_runs;
-        print_matrix(:, 4) = ber_sum_ask100'./i_runs;
+        for i_comp = 1 : num_comp
+        print_matrix(:, i_comp + 1) = ber_sum(i_comp, :)'./i_runs;
+        end
         disp(' ');
         disp(['Current run_time = ' num2str(i_runs)]);
         disp('ebno       ber_ask30 ber_ask80 ber_ask100');
         disp(num2str(print_matrix, '%.6f '));
-
+    % Moniter
         monitor_matrix(1,i_runs / print_resolution) = print_matrix(3,2)';   %调制深度越浅，信噪比越低，随机性越强
         set(hPlot30, 'YData', monitor_matrix(1,:));
 %         set(hPlot80, 'YData', print_matrix(:, 3)');
@@ -64,18 +85,21 @@ for i_runs = 1 : max_runs
     end
 end
 
-ber_avg_ask30 = ber_sum_ask30 / max_runs;
-ber_avg_ask80 = ber_sum_ask80 / max_runs;
-ber_avg_ask100 = ber_sum_ask100 / max_runs;
 
+%% Final Result
+ber_avg = zeros(num_comp, length(EbNo_dB_vec));
+for i_comp = 1 : num_comp      %这里用i_comp1是为了避免重复使用前面用过的i_comp，但我也不确定这里重复使用会不会出问题
+    ber_avg(i_comp, :) = ber_sum(i_comp, :) / max_runs;
+end
+
+markers = {'o-', 's-', '^-', 'd-', 'p-', 'h-', '+-'};
 figure;
-% semilogy(EbNo_dB_vec, ber_avg_ask30, 'o-', EbNo_dB_vec, ber_avg_ask80, 's-', EbNo_dB_vec, ber_avg_ask100, '^-');
 hold on;    %hold on启动图形保持，当前的普通坐标轴也会被保持，semilogy将无法改变坐标轴为对数坐标.
-semilogy(EbNo_dB_vec, ber_avg_ask30, 'o-');
-semilogy(EbNo_dB_vec, ber_avg_ask80, 's-');
-semilogy(EbNo_dB_vec, ber_avg_ask100, '^-');
+for i_comp = 1 : num_comp 
+    semilogy(EbNo_dB_vec, ber_avg(i_comp, :), markers{i_comp});     % 笔记： {}提取的是单元格内容，()提取的是一个单元格数组的子集。若使用了markers(i)，则marker的类型将是cell而非char
+end
 hold off;
-legend('ASK 调制深度 30%', 'ASK 调制深度 80%', 'ASK 调制深度 100%');
+legend('ASK 调制深度 30%', 'ASK 调制深度 80%', 'ASK 调制深度 100%', 'legend4');
 title('不同 ASK 调制深度下的误码率性能');
 xlabel('Eb/No (dB)');
 ylabel('误码率 (BER)');
